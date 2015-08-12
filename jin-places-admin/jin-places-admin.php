bae94edfc4d62497c794ccc8bbd727ead68ffcbf
Jinglero <jinglero@gmail.com>
2015-08-12 00:39:33 -0300

    First Commit 0.0.1
    Places es un plugin para incorporar un mapa con un listado de lugares
    en wordpress.

---
+++ jin-places-admin/jin-places-admin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is the main jin-places-admin plugin file, here we declare and call the important stuff for admin management
+ * @author Pedro García <jinglero@gmail.com>
+ * @since 0.0.2
+ * @package jin-places
+ */
+
+if (!defined('ABSPATH'))
+	exit; // Exit if accessed directly
---
+++ jin-places.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This is the main jin-places plugin file, here we declare and call the important stuff
+ * @author Pedro García <jinglero@gmail.com>
+ * @since 0.0.1
+ * @package jin-places
+ */
+
+/* 
+Plugin Name: Places on wordpress
+Plugin URI: http://jinglero.com/
+Description: Places on google maps for wordpress.
+Version: 0.0.1
+Author: GeoDirectory
+Author URI: http://jinglero.com/
+Requires at least: 4.2
+Tested up to: 4.2
+*/
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) )
+	die( 'No script kiddies please!' );
+
+//Definiciones de Constantes
+define( 'JINP_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+if(is_admin()){
+	//Incluyo admin
+	include(JINP_PATH.'/jin-places-admin/jin-places-admin.php');
+}
+
+if(!is_admin()){
+	
+}
+
